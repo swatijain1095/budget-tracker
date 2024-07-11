e2293e52d29478387567d55e609db69b2bcf3649
Swati Jain <jain.swati71@gmail.com>
2024-07-11 18:17:05 +0200

    installed lucide-react, shadcn-ui and also added icon and app name in sign-up/in page

---
+++ components/Logo.tsx
@@ -0,0 +1,15 @@
+import { PiggyBank } from "lucide-react";
+import React from "react";
+
+function Logo() {
+  return (
+    <a href="/" className="flex flex-row items-center justify-center gap-2">
+      <PiggyBank className="stroke h-12 w-12 stroke-fuchsia-400 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-purple-400 to-fuchsia-400 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Budget Tracker
+      </p>
+    </a>
+  );
+}
+
+export default Logo;
