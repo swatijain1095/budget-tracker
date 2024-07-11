89dc06309fa9f8c246624e3c79a7394a8c8ec6f1
Swati Jain <jain.swati71@gmail.com>
2024-07-11 13:43:46 +0200

    added sign-in and sign-up pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+
+function layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-center">
+      layout
+      <div className="mt-12">{children}</div>
+    </div>
+  );
+}
+
+export default layout;
