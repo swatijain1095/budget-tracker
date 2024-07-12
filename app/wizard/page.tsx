7373b939636f31d069a8467bd476cd3f8eb40e7c
Swati Jain <jain.swati71@gmail.com>
2024-07-12 19:20:20 +0200

    updated page file, added currencyComboBox component and currencies & use-media-query file

--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -1,3 +1,4 @@
+import { CurrencyComboBox } from "@/components/CurrencyComboBox";
 import Logo from "@/components/Logo";
 import { Button } from "@/components/ui/button";
 import {
@@ -22,12 +23,13 @@ async function page() {
   return (
     <div className="container flex max-w-2xl flex-col items-center justify-between gap-4">
       <h1 className="text-center text-3xl">
-        Welcome,<span className="ml-2 font-bold">{user.firstName}! 👋</span>
+        Welcome,
+        <span className="ml-2 font-bold">{user.firstName}! 👋</span>
       </h1>
-      <h2 className="mt-4 text-center text-base text-muted-foreground">
+      <h2 className="mt-1 text-center text-base text-muted-foreground">
         Let&apos;s get started by setting up your currency
       </h2>
-      <h3 className="mt-2 text-center text-sm text-muted-foreground">
+      <h3 className="mt-0.1 text-center text-sm text-muted-foreground">
         You can change these settings any time
       </h3>
       <Separator />
@@ -38,7 +40,9 @@ async function page() {
             Set your default currency for transactions
           </CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <CurrencyComboBox />
+        </CardContent>
       </Card>
       <Separator />
       <Button className="w-full" asChild>
---
+++ lib/currencies.ts
@@ -0,0 +1,9 @@
+export const Currencies = [
+  { value: "Rupee", label: "₹ Rupee", locale: "en-IN" },
+  { value: "USD", label: "$ Dollar", locale: "en-US" },
+  { value: "EUR", label: "€ Euro", locale: "de-DE" },
+  { value: "NOK", label: "kr NOK", locale: "no-NO" },
+  { value: "GBP", label: "£ Pound", locale: "en-GB" },
+];
+
+export type Currency = (typeof Currencies)[0];
