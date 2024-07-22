46b63c9545cf229914c6c9508e5ce4ded9f887a1
Swati Jain <jain.swati71@gmail.com>
2024-07-22 20:29:13 +0200

    added category section and progress bars

--- components/Overview.tsx
+++ components/Overview.tsx
@@ -7,6 +7,7 @@ import { DateRangePicker } from "./ui/date-range-picker";
 import { MAX_DATE_RANGE_DAYS } from "@/lib/constants";
 import { toast } from "sonner";
 import StatsCards from "./StatsCards";
+import CategoriesStats from "@/app/(dashboard)/_components/CategoriesStats";
 
 function Overview({ userSettings }: { userSettings: UserSettings }) {
   const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
@@ -41,6 +42,11 @@ function Overview({ userSettings }: { userSettings: UserSettings }) {
       </div>
       <div className="container flex w-full flex-col gap-2">
         <StatsCards
+          userSettings={userSettings}
+          from={dateRange.from}
+          to={dateRange.to}
+        />
+        <CategoriesStats
           userSettings={userSettings}
           from={dateRange.from}
           to={dateRange.to}
--- components/StatsCards.tsx
+++ components/StatsCards.tsx
@@ -101,7 +101,7 @@ const StatCard = ({
   return (
     <Card className="flex h-24 w-full items-center gap-2 p-4">
       {icon}
-      <div className="flex flex-col gap-0">
+      <div className="flex flex-col items-start gap-0">
         <p className="text-muted-foreground">{title}</p>
         <CountUp
           preserveValue
