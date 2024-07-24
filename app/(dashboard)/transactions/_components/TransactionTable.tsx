abbeccc80c452eccf2775257f7680a757984fae9
Swati Jain <jain.swati71@gmail.com>
2024-07-24 22:48:06 +0200

    added transactions and transactionTable files

---
+++ app/(dashboard)/transactions/_components/TransactionTable.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+interface Props {
+  from: Date;
+  to: Date;
+}
+
+function TransactionTable({ from, to }: Props) {
+  return <div>TransactionTable</div>;
+}
+
+export default TransactionTable;
